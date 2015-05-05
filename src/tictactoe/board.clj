2f878abbb0e3a71372031b1dcf31c9943058c9fa
calvached <calvache.diana@gmail.com>
2015-05-05 15:06:55 -0500

    Adding gameboard features

---
+++ src/tictactoe/board.clj
@@ -0,0 +1,40 @@
+(ns tictactoe.board
+  (:gen-class))
+
+(defn raise-to-power
+  [base exponent]
+  (reduce * (repeat exponent base))
+)
+
+(defn gameboard
+  [size]
+  (into [] (repeat (raise-to-power size 2) " "))
+)
+
+(defn index-of
+  [location]
+  (- location 1)
+)
+
+(defn valid-placement?
+  [location board]
+  (= (get board location) " ")
+)
+
+(defn place-piece
+  [location piece board]
+  (if
+    (valid-placement? (index-of location) board)
+    (assoc board (index-of location) piece)
+    board)
+)
+
+(defn available-cells
+  [board]
+  (remove nil?
+    (into []
+      (map-indexed
+        (fn [i cell]
+          (if (= cell " ") i))
+        board)))
+)
---
+++ test/tictactoe/board_test.clj
@@ -0,0 +1,44 @@
+(ns tictactoe.board-test
+  (:require [clojure.test :refer :all]
+            [tictactoe.board :refer :all]))
+
+(deftest board-test
+  (testing "it creates a board with 9 cells"
+    (is (= [" " " " " "
+            " " " " " "
+            " " " " " "] (gameboard 3)))
+  )
+
+  (testing "it creates a board with 25 cells"
+    (is (= [" " " " " " " " " "
+            " " " " " " " " " "
+            " " " " " " " " " "
+            " " " " " " " " " "
+            " " " " " " " " " "] (gameboard 5)))
+  )
+
+  (testing "returns true if cell is not occupied"
+    (is (= true
+           (valid-placement? 2 [" " "X" " " " " " " " " " " " " " "])))
+  )
+
+  (testing "returns false if cell is occupied"
+    (is (= false
+           (valid-placement? 1 [" " "X" " " " " " " " " " " " " " "])))
+  )
+
+  (testing "it places a piece on the board"
+    (is (= [" " " " " " " " "X" " " " " " " " "]
+           (place-piece 5 "X" [" " " " " " " " " " " " " " " " " "])))
+  )
+
+  (testing "it does not place a piece if a piece exists in a cell"
+    (is (= ["O" " " " " " " "X" " " " " " " " "]
+           (place-piece 1 "X" ["O" " " " " " " "X" " " " " " " " "])))
+  )
+
+  (testing "returns a list of indices for all available cells"
+    (is (= [2 5 6 7 8]
+           (available-cells ["X" "O" " " "O" "X" " " " " " " " "])))
+  )
+)
